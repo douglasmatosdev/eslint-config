9008098103e2a4a13bc3420063ef15bf1f3aa055
Douglas Matos da Silva <douglasmatosdev@gmail.com>
2023-11-25 14:12:57 -0300

    Update eslint.js add support to files .js and .jsx

--- eslint.js
+++ eslint.js
@@ -44,7 +44,7 @@ module.exports = {
     overrides: [
         {
             // enable the rule specifically for TypeScript files
-            files: ["*.ts", "*.tsx"],
+            files: ["*.ts", "*.tsx", ".js", ".jsx"],
             rules: {
                 "@typescript-eslint/explicit-module-boundary-types": ["error"],
                 "@typescript-eslint/explicit-function-return-type": ["warn", {
